03cf5c91df8a5ec75586f1e59fafe0d8ca08824b
Rafael <rafaelfferreira224@gmail.com>
2025-04-22 20:47:46 -0300

    adicionado sistema para exibir materias na tabela
    o sistema esta incompleto, exibe as materias mas não as exclui caso desmarcadas e não tem verificação de choque de horario

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let MATERIASMARCADAS = []
+
 function coletarJson(){
   return fetch('./dados.json')
   .then(response => {
@@ -13,21 +15,77 @@ function coletarJson(){
 
 }
 
+
+function removerMateria(materia){
+  if(MATERIASMARCADAS.length !== 0)
+    MATERIASMARCADAS.splice(materia, 1, 0)
+}
+
+function adicionarMateria(materia){
+  MATERIASMARCADAS.push(materia)
+}
+
+function verificar(check, materia){
+  if(check.checked){
+    adicionarMateria(materia)
+  }
+  else{
+    removerMateria(materia)
+  }
+  marcarMateria()
+}
+
+
+async function marcarMateria(){  
+  MATERIASMARCADAS.forEach(materia =>{
+    let idReferencia = []
+    for(let i = 0; i < materia.horarios.length; i++){
+      idReferencia.push(materia.horarios[i])
+    }
+
+    idReferencia.forEach(item =>{
+      let horario = document.getElementById(item)
+      if(horario){
+        horario.textContent = materia.nome
+      }
+      else{
+        horario.textContent = ""
+      }
+    })
+  })
+  
+
+}
+
 async function exibirMaterias(){
   let area = document.getElementById("materias")
   let conteudo = await coletarJson()
   console.log(conteudo);
   
   conteudo.forEach(element => {
+    const materia = {
+      nome: element.materia,
+      horarios: element.horarios
+    }
     let div = document.createElement("div")
+    
     let nome = document.createElement("span")
     nome.innerText = element.materia
+    
     let check = document.createElement("input")
     check.type = "checkbox"
+    check.addEventListener('change', function (){
+      verificar(this, materia)
+    })
+    
     div.appendChild(nome)
     div.appendChild(check)
     area.appendChild(div)
   });
+
+ 
 }
+
+// marcarMateria()
 
 document.addEventListener("DOMContentLoaded",exibirMaterias)
