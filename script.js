c8ac0eb17f5a6d107e003082fba207c842079a56
Rafael <rafaelfferreira224@gmail.com>
2025-04-22 18:46:08 -0300

    as matérias são exibidas em uma lista lateral

--- script.js
+++ script.js
@@ -1,33 +1,33 @@
 function coletarJson(){
-    fetch('./dados.json')
+  return fetch('./dados.json')
   .then(response => {
     if (!response.ok) {
       throw new Error('Erro ao carregar o arquivo JSON');
     }
     return response.json();  // Converte a resposta para um objeto JavaScript
   })
-  .then(data => {
-    console.log(data);  // Agora você pode trabalhar com o objeto JavaScript
-  })
+  
   .catch(error => {
     console.error('Erro:', error);
   });
 
 }
 
-function exibirMaterias(){
-    let area = document.getElementById("materias")
-    let conteudo = coletarJson()
-
-    conteudo.forEach(element => {
-        let div = document.createElement("div")
-        let nome = document.createElement("span")
-        nome.innerText = element.materia
-        let check = document.createElement("input").type = "checkbox"
-        div.appendChild(nome)
-        div.appendChild(check)
-        area.appendChild(div)
-    });
+async function exibirMaterias(){
+  let area = document.getElementById("materias")
+  let conteudo = await coletarJson()
+  console.log(conteudo);
+  
+  conteudo.forEach(element => {
+    let div = document.createElement("div")
+    let nome = document.createElement("span")
+    nome.innerText = element.materia
+    let check = document.createElement("input")
+    check.type = "checkbox"
+    div.appendChild(nome)
+    div.appendChild(check)
+    area.appendChild(div)
+  });
 }
 
 document.addEventListener("DOMContentLoaded",exibirMaterias)
